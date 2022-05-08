4e14fca8991d2c0eace14a066d2b275b9abe5a91
kargo <mahmudovarahide@gmail.com>
2022-05-08 23:30:44 +0400

    array

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var rahide = 5;
-
+const deger = 5;
 
 
 //Primite================//primitvler object deyil
@@ -41,3 +41,38 @@ let voteable = (age !== 18) ?
 console.log(voteable)
 
 //!== beraber deyilse
+
+
+//array methods
+
+let arrMethod = ["rahide", "nazrin", "zakir"]
+    // arrMethod.push("amil", "elgun");//arrayin icine push edir gonderir melumati sona
+    // arrMethod.unshift("alma")//en qabag elavee edir datanii
+    // arrMethod.pop(); //en axirinci elemti cixardir
+    // arrMethod.shift()//shif en basdan datani cixardir
+    // arrMethod.sort()  elifba sirasi ile dzur
+
+console.log(arrMethod)
+
+//esc5
+// const arry = [1, 3, 4, 5, 7, 9, 61, 64, 55];
+// const filterArry = arry.filter(function(e) {
+//     return e % 2 == 0;
+// });
+// console.log(filterArry)
+
+
+//filter esc6
+// let filterArryESC6 = arry.filter(e => e % 1 == 0);
+// console.log(filterArry)
+
+
+let text = "";
+var arrrrr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+for (i = 1; i < arrrrr.length; i++) {
+    arrrrr.filter(function(e) {
+        return i > 3;
+    });
+    text += (" " + i);
+}
+console.log(text)
