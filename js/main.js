cea6c182d23add261a780f2df17118e9b00ee6b6
kargo <mahmudovarahide@gmail.com>
2022-05-08 21:51:10 +0400

    3

---
+++ js/main.js
@@ -0,0 +1,43 @@
+var rahide = 5;
+
+
+
+//Primite================//primitvler object deyil
+let nezrin = 3;
+const zakir = 0;
+let toplama = nezrin + " " + zakir;
+console.log(toplama)
+const dataTypess = true;
+// null
+var nulum = null;
+var serdar;
+console.log(serdar);
+console.log(Math.random(zakir))
+
+
+
+//=======================
+
+
+//Non primite===============================//object
+
+//function,array
+
+var arry = ["rahide", "nezrin", 3]
+var arry = ["nuray", "resad", 4]
+console.log(arry)
+console.log(arry.length)
+console.log(arry[1])
+
+//====================
+
+
+
+let age = 19;
+let voteable = (age !== 18) ?
+    true : //true
+    false; //false//ternary opeartors
+
+console.log(voteable)
+
+//!== beraber deyilse
